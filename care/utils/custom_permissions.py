d274955d48950b31b06c5dd8af8e5f5b662b36da
prafful <praffulsharma1230@gmail.com>
2024-11-06 18:25:15 +0530

    updated isSuperUser permission class

--- care/utils/custom_permissions.py
+++ care/utils/custom_permissions.py
@@ -3,4 +3,9 @@ from rest_framework.permissions import BasePermission
 
 class IsSuperUser(BasePermission):
     def has_permission(self, request, view):
-        return request.user and request.user.is_superuser
+        return bool(
+            request.user
+            and request.user.is_authenticated
+            and request.user.is_active
+            and request.user.is_superuser
+        )
